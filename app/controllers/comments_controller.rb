f6d2aa5e668f4f6bbe9a837abf2b272982f6cd55
jaxklin <jaxklin@gmail.com>
2016-06-19 21:28:02 +0800

    fix current_user

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:content))
+    
     @comment.user = current_user
     if @comment.save
       redirect_to post_path(@post)
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ActiveRecord::Base
-	belongs_to :Post
-    belongs_to :User
+	belongs_to :post
+    belongs_to :user
     validates :content, presence: true, length: {minimum: 5}
-    
+
 end
